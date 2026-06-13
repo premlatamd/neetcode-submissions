320686559450174f4a19e040674c06ba0f77a150
premlatamd <118017441+premlatamd@users.noreply.github.com>
2026-06-14 02:27:49 +0530

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.py
@@ -0,0 +1,17 @@
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k %= len(nums)
+        nums[:] = nums[len(nums)-k:] + nums[:len(nums)-k]
+        
+            
+
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
