b2e8de01b4ddcff829a1161bc5294ed09fded658
premlatamd <118017441+premlatamd@users.noreply.github.com>
2026-07-11 12:06:11 +0530

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        for i in s:
+            if s.count(i)!=t.count(i):
+                return False
+        return True
+
+        
+
+        """if len(s) != len(t):
+            return False
+
+        for i in s:
+            c = 0
+
+            for key, j in enumerate(t):
+                if j == i:
+                    t = t[:key] + t[key+1:]
+                    c = 1
+                    break
+
+            if c == 0:
+                return False
+
+        return t == """""
