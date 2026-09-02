09dfad831b92fd6c046728cf43ef08d194710d5e
premlatamd <118017441+premlatamd@users.noreply.github.com>
2026-09-02 12:34:50 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        def encode(arr):
+            i=0
+            total=0
+            while arr!=[]:
+                m=arr.pop()
+                total+=m*(10**i)
+                i+=1
+
+            return total
+        
+        def decode(number):
+            n=number
+            print(n)
+            ans=[]
+            while n!=0:
+                r=n%10
+                n=n//10
+                ans.append(r)
+            ans.reverse()
+            return ans
+        
+        result=encode(digits)
+        ans=decode(result+1)
+        print(ans)
+
+        return ans
