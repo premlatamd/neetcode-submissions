adedb21c13a064cd681a95d741b6b9588c544a4f
premlatamd <118017441+premlatamd@users.noreply.github.com>
2026-09-05 21:41:56 +0530

    Add: sort-the-people - submission-2

---
+++ Data Structures & Algorithms/sort-the-people/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        arr=[-1]*len(names)
+        d={}
+        for key,i in zip(names,heights):
+            d[i]=key
+        l=sorted(list(d.keys()),reverse=True)
+        ans=[]
+        for i in l:
+            ans.append(d[i])
+        return ans
+        
