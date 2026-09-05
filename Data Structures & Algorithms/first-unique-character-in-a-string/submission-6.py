d30a23ee5bc89ea0fbb98c68ffc2cdd5f22c9cc8
premlatamd <118017441+premlatamd@users.noreply.github.com>
2026-09-05 20:23:47 +0530

    Add: first-unique-character-in-a-string - submission-6

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d={}
+        visited=set()
+        for i in range(len(s)):
+            if s[i] in visited and s[i] in d:
+                del d[s[i]]
+            if s[i] not in d and s[i] not in visited:
+                d[s[i]]=i
+                visited.add(s[i])
+           
+        print(d)
+        mini=float("inf")
+        for key ,i in d.items():
+            mini=min(mini,i)
+       
+        if mini!=float("inf"):
+            return mini
+        
+        return -1
+
+        
+        
